b736940e070dd7c2d41638730956456c6baa1b4f
Igor Kaplanovic <igor.kaplanovic@gmail.com>
2014-06-08 20:59:27 +0200

    Added script for fetching data.

---
+++ fetch.R
@@ -0,0 +1,2 @@
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip', destfile='household_power_consumption.zip', method='curl')
+unzip('household_power_consumption.zip')
